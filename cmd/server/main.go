1bd64b75eeffca5e9e17d26ae9dffb5acf46c2d5
Sahil2k07 <sahilsharma2k07@gmail.com>
2025-08-09 04:20:18 +0530

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+
+	e.Logger.Fatal(e.Start(":5000"))
+}
